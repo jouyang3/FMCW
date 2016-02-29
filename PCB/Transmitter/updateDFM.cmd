cafbb153497e27c80ea1cabc00db693a1f297b7a
Jun Ouyang <jdouyang@ucdavis.edu>
2016-02-28 22:25:26 -0800

    Fixed solder mask on VCO and PLL. Created Constraint File for BAC. Created updateDFM.cmd script to allow automated copy of files into the dfm folder.

---
+++ PCB/Transmitter/updateDFM.cmd
@@ -0,0 +1,15 @@
+mkdir dfm
+del dfm.zip
+copy /Y art_param.txt dfm\
+copy /Y BOTTOM.art dfm\
+copy /Y DRILL.art dfm\
+copy /Y eco.txt dfm\
+copy /Y nc_param.txt dfm\
+copy /Y ncdrill.log dfm\
+copy /Y OUTLINE.art dfm\
+copy /Y photoplot.log dfm\
+copy /Y PLL_VCO_FINAL-1-2.drl dfm\
+copy /Y SILK.art dfm\
+copy /Y SM_BOT.art dfm\
+copy /Y SM_TOP.art dfm\
+copy /Y TOP.art dfm\
